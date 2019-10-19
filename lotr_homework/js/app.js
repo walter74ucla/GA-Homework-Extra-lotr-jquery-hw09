20de8b116298f943c8adcef8337de1a1d5f7cd25
Walter Sylvester <walter_sylvester@msn.com>
2019-10-19 08:25:26 -0600

    Chapter 8 complete - The Fellowship is created

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -205,12 +205,14 @@ $('aside ul li:eq(3)').text('Aragorn');
 const forgeTheFellowShip = () => {
 
   // 1. create a new div with an id 'the-fellowship'
-
+  const $div = $('<div/>');
+  $div.attr('id', 'the-fellowship').append(`<h1>The Fellowship</li>`);
   // 2. add an h1 with the text 'The Fellowship' to this new div
 
   // 3. append the fellowship to middle-earth
-
+  $('#middle-earth').append($div);
   // 4. add the unordered lists of hobbits and buddies to 'the-fellowship'
+  $('#the-fellowship').append($('#Rivendell>ul'), $('aside>ul'));
 
 };
 
